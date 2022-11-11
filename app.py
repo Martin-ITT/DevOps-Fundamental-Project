5f763aca3d1f0c3502d157ea2c3ac093738ae8c1
Martin-ITT <martinusko@gmail.com>
2022-11-11 21:59:07 +0000

    final ERD and modules implemented. DB created, Read from DB successfull

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import date
-import random
+
 
 app = Flask(__name__)
 
@@ -15,8 +14,7 @@ db = SQLAlchemy(app)
 # DB Models
 class Students(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    enrolments = db.relationship('Enrolments', backref='students') # FK many to many Enrolments
-    s_grades = db.relationship('Grades', backref='students') # FK ref Grades
+    enrolments = db.relationship('Enrolments', backref='students') # FK one to many Enrolments
     student_fname = db.Column(db.String(30), nullable=False)
     student_lname = db.Column(db.String(30), nullable=False)
     student_address1 = db.Column(db.String(50), nullable=False)
@@ -33,7 +31,6 @@ class Students(db.Model):
 class Tutors(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     t_modules = db.relationship('Modules', backref='tutors') # FK ref Modules
-    t_grades = db.relationship('Grades', backref='tutors') # FK ref Grades
     tutor_fname = db.Column(db.String(30), nullable=False)
     tutor_lname = db.Column(db.String(30), nullable=False)
     tutor_address1 = db.Column(db.String(50), nullable=False)
@@ -59,96 +56,18 @@ class Enrolments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     enrol_student_id = db.Column('student_id', db.Integer, db.ForeignKey('students.id')) # FK students
     enrol_module_id = db.Column('modules_id', db.Integer, db.ForeignKey('modules.id')) # FK modules
-    enrol_grade_id = db.relationship('Grades', backref='enrolments', uselist=False) # FK ref Grades
-    academic_year = db.Column(db.Date, nullable=False)
-
-
-class Grades(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    grades_student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False) # FK Students 
-    grades_tutor_id = db.Column(db.Integer, db.ForeignKey('tutors.id'), nullable=False) # FK Tutors
-    grades_enrol_id = db.Column(db.Integer, db.ForeignKey('enrolments.id'), nullable=False)
     academic_year = db.Column(db.Date, nullable=False)
     ca1_score = db.Column(db.Integer)
     ca2_score = db.Column(db.Integer)
     exam_score = db.Column(db.Integer)
 
 
-db.drop_all()
-db.create_all()
-
-# add to DB
-for i in range(1,11):
-    student = Students(student_fname = 'fname' + str(i),
-    student_lname = 'lname' + str(i),
-    student_address1 = 'address1 ' + str(i),
-    student_address2 = 'address2 ' + str(i),
-    student_city = 'city' + str(i),
-    student_county = 'county' + str(i),
-    student_phone =  random.randint(871111111, 879999999),
-    student_DOB = date(year = random.randint(1960, 2004), month=random.randint(1, 12), day=random.randint(1, 28)),
-    student_email = 'email_' + str(i) + '@gmail.com',
-    student_password_ff = 'hash' + str(i),
-    authority_lvl = 0)
-
-    db.session.add(student)
-
-
-for i in range(1,6):
-    tutor = Tutors(tutor_fname = 'fname' + str(i),
-    tutor_lname = 'lname' + str(i),
-    tutor_address1 = 'address1 ' + str(i),
-    tutor_address2 = 'address2 ' + str(i),
-    tutor_city = 'city' + str(i),
-    tutor_county = 'county' + str(i),
-    tutor_phone =  random.randint(871111111, 879999999),
-    tutor_DOB = date(year = random.randint(1960, 2004), month=random.randint(1, 12), day=random.randint(1, 28)),
-    tutor_email = 'email_' + str(i) + '@gmail.com',
-    tutor_password_ff = 'hash' + str(i),
-    authority_lvl = 1)
-
-    db.session.add(tutor)
-
-for i in range(1,6):
-    module = Modules(module_name = 'module ' + str(i),
-    m_tutor_id = random.randint(1, 5), # foreign key !!!!!!!!!!!!!!!
-    description = 'module description ' + str(i))
-
-    db.session.add(module)
-
-for i in range(1,51):
-    enrolment = Enrolments(enrol_student_id = random.randint(1, 10), # foreign keys !!!!!!!!!!!!!!
-    enrol_module_id = random.randint(1, 5),
-    academic_year = date(year=2022, month=9, day=1))
-
-    db.session.add(enrolment)
-
-for i in range(1, 21):
-    grade = Grades(grades_student_id = random.randint(1, 10), # foreign keys !!!!!!!!!!!!!!
-    grades_tutor_id = random.randint(1, 5),
-    grades_enrol_id = random.randint(1, 50),
-    academic_year = date(year=2022, month=9, day=1),
-    ca1_score = random.randint(0, 100),
-    ca2_score = random.randint(1, 100),
-    exam_score = random.randint(1, 100))
-
-    db.session.add(grade)
-
-
-db.session.commit()
-
-
-'''
-testuser = Users(first_name='Grooty',last_name='Toot') # Extra: this section populates the table with an example entry
-db.session.add(testuser)
-db.session.commit()
-'''
-
-
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'This is the home page'
+    txt = Students.query.first()
+    print(txt)
+    return f"This is student: {txt.student_phone}"
 
 @app.route('/about')
 def about():
--- create.py
+++ create.py
@@ -1,3 +1,58 @@
 from app import db
+from datetime import date
+import random
+from app import Students, Tutors, Enrolments, Modules
 
+db.drop_all()
 db.create_all()
+
+# add to DB
+for i in range(1,11):
+    student = Students(student_fname = 'fname' + str(i),
+    student_lname = 'lname' + str(i),
+    student_address1 = 'address1 ' + str(i),
+    student_address2 = 'address2 ' + str(i),
+    student_city = 'city' + str(i),
+    student_county = 'county' + str(i),
+    student_phone =  random.randint(871111111, 879999999),
+    student_DOB = date(year = random.randint(1960, 2004), month=random.randint(1, 12), day=random.randint(1, 28)),
+    student_email = 'email_' + str(i) + '@gmail.com',
+    student_password_ff = 'hash' + str(i),
+    authority_lvl = 0)
+
+    db.session.add(student)
+
+
+for i in range(1,6):
+    tutor = Tutors(tutor_fname = 'fname' + str(i),
+    tutor_lname = 'lname' + str(i),
+    tutor_address1 = 'address1 ' + str(i),
+    tutor_address2 = 'address2 ' + str(i),
+    tutor_city = 'city' + str(i),
+    tutor_county = 'county' + str(i),
+    tutor_phone =  random.randint(871111111, 879999999),
+    tutor_DOB = date(year = random.randint(1960, 2004), month=random.randint(1, 12), day=random.randint(1, 28)),
+    tutor_email = 'email_' + str(i) + '@gmail.com',
+    tutor_password_ff = 'hash' + str(i),
+    authority_lvl = 1)
+
+    db.session.add(tutor)
+
+for i in range(1,6):
+    module = Modules(module_name = 'module ' + str(i),
+    m_tutor_id = random.randint(1, 5),
+    description = 'module description ' + str(i))
+
+    db.session.add(module)
+
+
+for i in range(1,11):
+    for x in range (1, random.randint(2,5)):
+        enrolment = Enrolments(enrol_student_id = i,
+        enrol_module_id = x,
+        academic_year = date(year=2022, month=9, day=1))
+
+        db.session.add(enrolment)
+
+
+db.session.commit()
